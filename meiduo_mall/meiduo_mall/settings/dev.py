a744c1ee541863dd1c3c87c79261b04a15f3f927
JavsShao <javs_shao@163.com>
2019-04-22 08:27:11 +0800

    定义用户模型类与配置

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -193,6 +193,9 @@ LOGGING = {
     }
 }
 
+# 指明自定义的用户模型类
+AUTH_USER_MODEL = 'users.User'
+
 REST_FRAMEWORK = {
     # 异常处理
     'EXCEPTION_HANDLER': 'meiduo_mall.utils.exceptions.exception_handler',
