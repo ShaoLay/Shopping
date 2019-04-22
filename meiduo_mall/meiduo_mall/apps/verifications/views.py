d6d3c4cca0279fe1c204d51eaf15421d9a816d8e
JavsShao <javs_shao@163.com>
2019-04-22 16:04:50 +0800

    图片验证码之前后端实现

---
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from django_redis import get_redis_connection
+from rest_framework.views import APIView
+
+from meiduo_mall.libs.captcha.captcha import captcha
+from verifications import constants
+
+
+class ImageCodeView(APIView):
+    """图片验证码"""
+    def get(self, request, image_code_id):
+
+        # 生成验证码图片
+        text, image = captcha.generate_captcha()
+
+        # 保存真实值
+        redis_conn = get_redis_connection('verify_codes')
+        redis_conn.setex("img_%s" % image_code_id, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+
+        # 返回图片
+        return HttpResponse(image, content_type='image/jpg')
+
+
