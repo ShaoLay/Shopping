7d2124517522aa4f38f2f05986297e27ef986933
JavsShao <javs_shao@163.com>
2019-04-29 10:54:05 +0800

    注册之前后端实现

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
 # Create your views here.
+from users import serializers
+
+
+class UserView(CreateAPIView):
+    """用户注册"""
+    serializer_class = serializers.CreateUserSerializer
