755dd93fce55fe8991a8cf9f227ff2e32eaf34a3
JavsShao <javs_shao@163.com>
2019-05-16 09:13:40 +0800

    商品详情页静态化

--- meiduo_mall/meiduo_mall/apps/goods/utils.py
+++ meiduo_mall/meiduo_mall/apps/goods/utils.py
@@ -1,18 +1,24 @@
 from collections import OrderedDict
-from django.conf import settings
-from django.template import loader
-import os
-import time
 
-from goods.models import GoodsChannel
-from .models import ContentCategory
+from .models import GoodsChannel
 
 
-def generate_static_index_html():
+def get_categories():
     """
-    生成静态的主页html文件
+    获取商城商品分类菜单
+    :return 菜单字典
     """
-    print('%s: generate_static_index_html' % time.ctime())
+    # 商品频道及分类菜单
+    # 使用有序字典保存类别的顺序
+    # categories = {
+    #     1: { # 组1
+    #         'channels': [{'id':, 'name':, 'url':},{}, {}...],
+    #         'sub_cats': [{'id':, 'name':, 'sub_cats':[{},{}]}, {}, {}, ..]
+    #     },
+    #     2: { # 组2
+    #
+    #     }
+    # }
     categories = OrderedDict()
     channels = GoodsChannel.objects.order_by('group_id', 'sequence')
     for channel in channels:
@@ -35,20 +41,4 @@ def generate_static_index_html():
             for cat3 in cat2.goodscategory_set.all():
                 cat2.sub_cats.append(cat3)
             categories[group_id]['sub_cats'].append(cat2)
-
-    # 广告内容
-    contents = {}
-    content_categories = ContentCategory.objects.all()
-    for cat in content_categories:
-        contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
-
-    # 渲染模板
-    context = {
-        'categories': categories,
-        'contents': contents
-    }
-    template = loader.get_template('index.html')
-    html_text = template.render(context)
-    file_path = os.path.join(settings.GENERATED_STATIC_HTML_FILES_DIR, 'index.html')
-    with open(file_path, 'w') as f:
-        f.write(html_text)
+    return categories
