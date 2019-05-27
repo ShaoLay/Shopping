784e84ee957af9569164a37f18d7a2ccf685fc5a
JavsShao <javs_shao@163.com>
2019-05-27 08:29:41 +0800

    商品列表前后端实现

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView
+
+from goods.models import SKU
+from goods.serializers import SKUSerializer
+
+
+class SKUListView(ListAPIView):
+    """
+    商品列表视图
+    """
+    serializer_class = SKUSerializer
+    # queryset = SKU.objects.filter(category=???)
+
+    # 排序
+    filter_backends = [OrderingFilter]
+    ordering_fields = ('create_time', 'price', 'sales')
+
+    # 分页, 全局设置
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return SKU.objects.filter(category_id=category_id, is_launched=True)
