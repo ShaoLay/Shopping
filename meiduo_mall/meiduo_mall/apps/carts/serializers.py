df85dc40f9a98dafb0a3a46c6b8872d98d5ea6d5
JavsShao <javs_shao@163.com>
2019-06-04 09:19:05 +0800

    查询购物车前后端实现

--- meiduo_mall/meiduo_mall/apps/carts/serializers.py
+++ meiduo_mall/meiduo_mall/apps/carts/serializers.py
@@ -22,4 +22,19 @@ class CartSerializer(serializers.Serializer):
 
         return data
 
+    class Meta:
+        model = SKU
+        fields = ('id', 'count', 'name', 'default_image_url', 'price', 'selected')
+
+class CartSKUSerializer(serializers.ModelSerializer):
+    """
+    购物车商品数据序列化器
+    """
+    count = serializers.IntegerField(label='数量')
+    selected = serializers.BooleanField(label='是否勾选')
+
+    class Meta:
+        model = SKU
+        fields = ('id', 'count', 'name', 'default_image_url', 'price', 'selected')
+
 
