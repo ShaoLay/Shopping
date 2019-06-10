d95faaa1fd5d42653de39084b86294ec2cc493cb
JavsShao <javs_shao@163.com>
2019-06-10 16:20:00 +0800

    获取支付宝支付链接地址接口实现

--- meiduo_mall/meiduo_mall/apps/payments/views.py
+++ meiduo_mall/meiduo_mall/apps/payments/views.py
@@ -1,3 +1,56 @@
+import os
+
+from alipay import AliPay
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.conf import settings
+
+from orders.models import OrderInfo
+
+
+class PaymentView(APIView):
+    """
+    获取支付链接
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, order_id):
+        user = request.user
+
+        try:
+            order = OrderInfo.objects.get(
+                order_id = order_id,
+                user = user,
+                pay_method=OrderInfo.PAY_METHODS_ENUM['ALIPAY']
+            )
+        except OrderInfo.DoerNotExit:
+            return Response({'message':'订单信息有误'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 向支付宝发起请求, 获取支付链接参数
+        alipay_client = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "keys/app_private_key.pem"),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                "keys/alipay_public_key.pem"),  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=settings.ALIPAY_DEBUG  # 默认False  是否是沙箱环境
+        )
+        # 电脑网站支付, 需要跳转到https://openapi.alipay.com/gateway.do? + order_string
+        order_string = alipay_client.api_alipay_trade_page_pay(
+            out_trade_no=order_id,  # 订单编号
+            total_amount=str(order.total_amount),  # 总金额
+            subject='美多商城订单%s' % order_id,
+            return_url="http://www.meiduo.site:8080/pay_success.html",
+            notify_url=None  # 可选, 不填则使用默认notify url
+        )
+        # 拼接支付链接网址
+        alipay_url = settings.ALIPAY_URL + '?' + order_string
+
+        # 返回
+        return Response({'alipay_url': alipay_url})
--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -331,6 +331,10 @@ HAYSTACK_CONNECTIONS = {
 # 当添加、修改、删除数据时，自动生成索引
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
+# 支付宝
+ALIPAY_APPID = '2016091600525753'
+ALIPAY_DEBUG = True
+ALIPAY_URL = 'https://openapi.alipaydev.com/gateway.do'
 
 
 
